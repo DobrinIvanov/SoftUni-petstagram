440c4ca58d0cc7bcfeb7e87f6ec54abf68d37da5
Dobrin Ivanov <dobsc2@gmail.com>
2022-10-22 13:35:02 +0300

    change 'list_display' for PetAdmin

--- petstagram/pets/admin.py
+++ petstagram/pets/admin.py
@@ -4,16 +4,7 @@ from petstagram.pets.models import Pet
 
 # Register your models here.
 
-<<<<<<< HEAD
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
-    pass
-=======
-admin.site.register(Pet)
-
-
-# @admin.register(Pet)
-# class PetAdmin(admin.ModelAdmin):
-#     pass
->>>>>>> 8e1afc06f53edfc287e72bbbbbf2140374ac116c
+    list_display = ("name", "slug")
