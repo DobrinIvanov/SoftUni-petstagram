f757159f47b1628302bda8ac415000280d1ff368
Dobrin Ivanov <dobsc2@gmail.com>
2022-10-22 13:30:10 +0300

    adding Photo model

--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+from petstagram.pets.models import Pet
+
+
 # Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField()
+    description = models.TextField(
+        max_length=10,
+        validators=(MinLengthValidator(10),),
+        blank=True,
+        null=True,
+    )
+    location = models.CharField(
+        max_length=30,
+        blanks=True,
+        null=True,
+    )
+    tagged_pets = models.ManyToManyField(
+        Pet,
+        blank=True,
+    )
+    date_of_publications = models.DateField(
+        auto_now=True,
+    )
