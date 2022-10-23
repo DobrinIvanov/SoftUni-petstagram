809a5532003d1c9a6c41e4f808d3c0e2d9cfac1a
Dobrin Ivanov <dobsc2@gmail.com>
2022-10-23 12:53:25 +0300

    create Like and Comment models

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from petstagram.photos.models import Photo
+
+
 # Create your models here.
+
+
+class Comment(models.Model):
+    MAX_COMMENT_LENGTH = 300
+
+    text = models.TextField(
+        max_length=MAX_COMMENT_LENGTH,
+    )
+    date_time_publication = models.DateTimeField(
+        auto_now_add=True,
+    )
+    to_photo = models.ForeignKey(
+        Photo,
+        on_delete=models.CASCADE,
+    )
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(
+        Photo,
+        on_delete=models.CASCADE,
+    )
